79965688b550299624a3b7c6905d60d609076812
MaianneThornton <maianne.thornton@gmail.com>
2021-09-23 09:21:35 -0400

    Updated answers while checking before class.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(warmHugs.replace("like","love"));
 let beenImpaled = "Oh, look at that. I've been impaled"
 console.log(beenImpaled);
 // 2b
-console.log(beenImpaled.slice(18));
+console.log(beenImpaled.slice(18)); 
 // 3a
 let dotDotDot = "...";
 console.log(dotDotDot);
@@ -17,23 +17,29 @@ console.log(dotDotDot);
 let skullBones = `I don't have a skull${dotDotDot} or bones.`
 console.log(skullBones);
 // 4
-console.log(Math.PI)
+console.log(Math.PI);
 // 5
-let randomNumber = Math.random();
-randomNumber *= 3;
-randomNumber = Math.ceil(randomNumber)
+// Option 1
+// let randomNumber = Math.random();
+// randomNumber *= 3;
+// randomNumber = Math.ceil(randomNumber)
+// console.log(randomNumber);
+
+// Option 2
+let randomNumber = Math.ceil(Math.random()*3);
 console.log(randomNumber);
 
 // BONUS TERRITORY
 // 6
-let letItGo = " Let It Go!"
-console.log(letItGo.toUpperCase().repeat(2));
+console.log(" Let It Go!".toUpperCase().repeat(2));
 // 7a
-let reindeers = "Reindeers are better than people."
+let reindeers = "Reindeers are better than people.";
 console.log(reindeers);
 // 7b
 reindeers = reindeers.replace(/ /g, "_");
 console.log(reindeers);
 // 8
-console.log(Math.sqrt(2));
+console.log(Math.SQRT2);
 // 9
+let newRandomNumber = Math.ceil(Math.random() * (24 - 7) + 6);
+console.log(newRandomNumber);
